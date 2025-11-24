29f0c578f21d568bcde19bcbca99915fd6a5be77
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2025-11-24 10:32:29 -0300

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,31 @@
 // @ts-check
 import { defineConfig, fontProviders } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [sitemap(), react()],
+  integrations: [react()],
 
-	vite: {
-		plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
 	},
 
-	experimental: {
-		fonts: [
-			{
-				provider: fontProviders.google(),
-				name: "Inter",
-				cssVariable: "--font-inter",
-			},
-			{
-				provider: fontProviders.google(),
-				name: "Science Gothic",
-				cssVariable: "--font-science-gothic",
-			},
-		],
+  adapter: vercel(),
+
+  experimental: {
+      fonts: [
+          {
+              provider: fontProviders.google(),
+              name: "Inter",
+              cssVariable: "--font-inter",
+          },
+          {
+              provider: fontProviders.google(),
+              name: "Science Gothic",
+              cssVariable: "--font-science-gothic",
+          },
+      ],
 	},
 });
